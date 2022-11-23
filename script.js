bbc274f482be830c74631bf764b08546e90d9f31
Wyllwyll <terry.boeuf@hotmail.fr>
2022-11-23 15:47:00 +0100

    ajout html + api js

---
+++ script.js
@@ -0,0 +1,3 @@
+const API_coord= "http://api.openweathermap.org/geo/1.0/direct?q={city name},{FR}&limit={limit}&appid={API key}"
+
+const API_villes= "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
